da9399a882368d818f72c8391f542d6112419d8a
johnvelis <john@johnvelis.com>
2020-03-18 11:58:34 -0400

    Add project files.

---
+++ Demo_Inheritance_GameObjects/GameObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Inheritance_GameObjects
+{
+    public abstract class GameObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Consumable { get; set; }
+
+        public GameObject()
+        {
+
+        }
+
+        public GameObject(int id, string name, string description, bool consumable = false)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Consumable = consumable;
+        }
+
+        //
+        // abstract methods 'must' be overriden in child class making it a requirement
+        //
+        public abstract string Use();
+
+        //
+        // virtual methods 'may' be overriden in the child class making the parent method a default
+        //
+        public virtual string Message()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+
+            sb.Append($"The {Name}: {Description} it is ");
+            sb.Append(Consumable ? "consumable" : "is not consumable");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Demo_Inheritance_GameObjects/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Inheritance_GameObjects
+{
+    public class Weapon : GameObject
+    {
+        public enum WeaponType
+        {
+            NONE,
+            MELEE,
+            RANGED,
+            DEFENSIVE
+        }
+
+        public WeaponType WeaponTypeName { get; set; }
+
+
+        public Weapon()
+        {
+
+        }
+
+        public Weapon(int id, string name, string description, bool consumable) :
+            base(id, name, description, consumable)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Consumable = consumable;
+        }
+
+        public override string Use()
+        {
+            switch (WeaponTypeName)
+            {
+                case WeaponType.NONE:
+                    return "You are using a weapon of unknown type.";
+
+                case WeaponType.MELEE:
+                    return $"You swing the {Name} with all of your might.";
+
+                case WeaponType.RANGED:
+                    return $"You step back and unleash the {Name}.";
+
+                case WeaponType.DEFENSIVE:
+                    return $"Bringing up the {}, you defend yourself gallantly.";
+                default:
+                    return "You are using a weapon of unknown type.";
+            }
+        }
+    }
+}
